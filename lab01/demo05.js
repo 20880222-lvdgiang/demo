f4b5a8049da904e9f179eda50f87ed5a2f50923e
Giang Le <wikihead99@gmail.com>
2022-03-13 01:41:23 +0700

    feature(lab01): ES - các phép toán và phép so sánh

---
+++ lab01/demo05.js
@@ -0,0 +1,31 @@
+/**
+ * Các phép toán và phép so sánh
+ */
+
+let num=100;
+console.log("num = ",num);
+console.log("num++ =>",num++); //xuất ra num rồi mới ++
+console.log("num = ",num);
+console.log("----------------------------");
+console.log("num = ",num);
+console.log("++num =>",++num); //++ trước rồi sau đó mới xuất ra num
+console.log("num = ",num);
+console.log("----------------------------");
+console.log("num = ",num);
+console.log("num-- =>",num--); //xuất ra num trước rồi mới --
+console.log("num = ",num);
+console.log("----------------------------");
+console.log("num = ",num);
+console.log("--num =>",--num); //-- trước rồi sau đó mới xuất ra num
+console.log("num = ",num);
+console.log("-------------------------------")
+console.log("num = ",num);
+console.log("num = num + 1 =>",num+1);
+console.log("num = num - 1 =>",num-1);
+console.log("num = num * 2 =>",num*2);
+console.log("num = num / 2 =>",num/2);
+console.log("num = num / 3 =>",num/3);
+console.log("num = num % 3 =>",num%3);
+console.log("num = num / 3 =>",parseInt(num/3));
+console.log("num = num / 3 =>",Math.floor(num/3));
+console.log("----------------------------");
