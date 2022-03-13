c3cb5f88aacb2c4e5ab38101928e00f3f957ac59
Giang Le <wikihead99@gmail.com>
2022-03-13 20:27:57 +0700

    feature(lab01): ES - Vòng lặp

---
+++ lab01/demo07.js
@@ -0,0 +1,24 @@
+/**
+ * Vòng lặp trong ES
+ */
+
+console.log("---for loop---");
+for(let i=1;i<=10;i++){
+    console.log(i);
+}
+
+console.log("---while loop---");
+let i=0;
+while(i<=10)
+{
+    console.log(i);
+    i+=2;
+}
+
+console.log("---do while loop---");
+let j=9;
+do{
+    console.log(j);
+    j-=2;
+}
+while(j>0);
